81d5a296478240175922567edf215b8ed70fe10a
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-04-29 13:08:58 +0530

    working on dynamic routes and advanced models

---
+++ Section 9 - Dynamic Routes & Advanced Models/Dynamic Routes & Advanced Models/controllers/admin.js
@@ -0,0 +1,34 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formCSS: true,
+    productCSS: true,
+    activeProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+
+  const product = new Product(title, imageUrl, price, description);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+      prods: products,
+    });
+  });
+};
