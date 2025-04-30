3587c936cfc9abddf7338705898c7e8b74ab4412
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-04-30 15:11:17 +0530

    completed with populating the data in the edit product form

--- Section 9 - Dynamic Routes & Advanced Models/Dynamic Routes & Advanced Models/controllers/admin.js
+++ Section 9 - Dynamic Routes & Advanced Models/Dynamic Routes & Advanced Models/controllers/admin.js
@@ -3,12 +3,10 @@ const Product = require("../models/product");
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
 
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formCSS: true,
-    productCSS: true,
-    activeProduct: true,
+    editing: false,
   });
 };
 
@@ -21,6 +19,25 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, price, description);
   product.save();
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit; // /admin/edit-product/24?edit=true
+  if (!editMode) {
+    res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
+  });
 };
 
 exports.getProducts = (req, res, next) => {
