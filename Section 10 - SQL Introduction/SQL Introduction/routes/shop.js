1336e801ea464b7cad7a511f2f0187fcbcfc91db
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-05-01 19:47:22 +0530

    sql introduction

---
+++ Section 10 - SQL Introduction/SQL Introduction/routes/shop.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const shopController = require("../controllers/shop");
+
+const router = express.Router();
+
+router.get("/", shopController.getIndex);
+
+router.get("/products", shopController.getProducts);
+
+router.get("/products/:productId", shopController.getProduct); // productId is a placeholder and can be used to get the product id from the URL
+
+router.get("/cart", shopController.getCart);
+
+router.post("/cart", shopController.postCart); // this is the route for the cart post request
+
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
+
+module.exports = router;
