86a9c6d19f76f36772693045509b7622fbe3eed0
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-05-01 15:05:42 +0530

    feature cart items and quantity being rendered

--- Section 9 - Dynamic Routes & Advanced Models/Dynamic Routes & Advanced Models/controllers/shop.js
+++ Section 9 - Dynamic Routes & Advanced Models/Dynamic Routes & Advanced Models/controllers/shop.js
@@ -34,9 +34,24 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Your Cart",
-    path: "/cart",
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (let product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cart.products.find((prod) => prod.id === product.id)) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+
+      res.render("shop/cart", {
+        pageTitle: "Your Cart",
+        path: "/cart",
+        products: cartProducts,
+      });
+    });
   });
 };
 
