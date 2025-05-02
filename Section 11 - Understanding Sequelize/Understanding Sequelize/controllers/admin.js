de6413a6bb0dc284f2c82b9a1aae23efe2bd1ab6
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-05-02 16:54:17 +0530

    inserting data and creating product

--- Section 11 - Understanding Sequelize/Understanding Sequelize/controllers/admin.js
+++ Section 11 - Understanding Sequelize/Understanding Sequelize/controllers/admin.js
@@ -16,11 +16,15 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const price = req.body.price;
 
-  const product = new Product(null, title, imageUrl, price, description);
-  product
-    .save()
-    .then(() => {
-      res.redirect("/");
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((result) => {
+      // console.log(result);
+      console.log("Created Product");
     })
     .catch((err) => {
       console.log(err);
