6886ab873fe567c7fcdc79221d1c735d897a4438
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-05-02 15:24:09 +0530

    starting with understanding squelize

---
+++ Section 11 - Understanding Sequelize/Understand Squelize/app.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// const { engine } = require("express-handlebars"); // Import express-handlebars
+const errorController = require("./controllers/error");
+const db = require("./util/database");
+
+const app = express();
+
+// Set up the view engine - for ejs - 3 lines below
+app.set("view engine", "ejs"); // Set the view engine to EJS -- ejs doesn't support layouts
+app.set("views", "views"); // Set the views directory
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+// db.execute("SELECT * FROM products")
+//   .then((result) => {
+//     console.log(result[0], result[1]);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });  // TESTING DB CONNECTION
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404); // 404 page
+
+app.listen(3000);
