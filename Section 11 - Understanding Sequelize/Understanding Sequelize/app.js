fbf737281a48fba2f957bf39ebdf54b623bcf89e
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-05-08 14:15:02 +0530

    Dummy user

--- Section 11 - Understanding Sequelize/Understanding Sequelize/app.js
+++ Section 11 - Understanding Sequelize/Understanding Sequelize/app.js
@@ -21,6 +21,10 @@ const User = require("./models/user");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1);
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -30,10 +34,23 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); // A produc
 User.hasMany(Product); // A user can have many products
 
 sequelize
-  .sync({ force: true }) // force: true will drop the table if it already exists
+  //   .sync({ force: true }) // force: true will drop the table if it already exists
+  .sync()
   .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Kaushik", email: "test@test.com" });
+    }
+    // return Promise.resolve(user);
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
+
   .catch((err) => {
     console.log(err);
   });
