79987b5a4a5f2818e9904791fcf20ba9c6c6c9e2
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-06-11 16:25:36 +0530

    Rendering products on Orders page

--- Section 11 - Understanding Sequelize/Understanding Sequelize/controllers/shop.js
+++ Section 11 - Understanding Sequelize/Understanding Sequelize/controllers/shop.js
@@ -195,15 +195,21 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    pageTitle: "Your Orders",
-    path: "/orders",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        pageTitle: "Your Orders",
+        path: "/orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    pageTitle: "Checkout",
-    path: "/checkout",
-  });
-};
+// exports.getCheckout = (req, res, next) => {
+//   res.render("shop/checkout", {
+//     pageTitle: "Checkout",
+//     path: "/checkout",
+//   });
+// };
