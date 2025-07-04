05d613b3ace4e05b7d141fdc13dc4e387b111799
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-04 15:51:10 +0530

    Using relations in mongoose by - ref

--- Section 13 - Working with Mongoose/Working with Mongoose/controllers/admin.js
+++ Section 13 - Working with Mongoose/Working with Mongoose/controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
+    userId: req.user, // This will add the entire user doc but mongoose will only pick the _id. Alternatively it can be written as - req.user_id
   });
 
   // req.user.createProduct();
