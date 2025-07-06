50a248543ff50728a909862a7eb678d8595d5eca
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-06 20:24:35 +0530

    Using the session middleware

--- Section 14 - Sessions & Cookies/Session and Cookies/controllers/auth.js
+++ Section 14 - Sessions & Cookies/Session and Cookies/controllers/auth.js
@@ -1,15 +1,19 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn =
-    req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+  //   const isLoggedIn =
+  //     req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  req.session.isLoggedIn = true;
+
+  //   res.setHeader("Set-Cookie", "loggedIn=true");
   //   res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10"); // Adding multiple
   //   res.setHeader("Set-Cookie", "loggedIn=true; Secure"); // For https
   //   res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly"); // for http
