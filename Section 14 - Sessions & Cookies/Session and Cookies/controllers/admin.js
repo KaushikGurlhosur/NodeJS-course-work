b14583edaf1e264376ee4b0a71e2b03ae2d87c79
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-06 19:12:53 +0530

    Setting a Cookie

--- Section 14 - Sessions & Cookies/Session and Cookies/controllers/admin.js
+++ Section 14 - Sessions & Cookies/Session and Cookies/controllers/admin.js
@@ -7,6 +7,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
+    isAuthenticated: req.isLoggedIn,
   });
 };
 
@@ -54,6 +55,7 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -107,6 +109,7 @@ exports.getProducts = (req, res, next) => {
         pageTitle: "Admin Products",
         path: "/admin/products",
         prods: products,
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {
--- Section 14 - Sessions & Cookies/Session and Cookies/controllers/error.js
+++ Section 14 - Sessions & Cookies/Session and Cookies/controllers/error.js
@@ -1,5 +1,9 @@
 exports.get404 = (req, res, next) => {
   //   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" }); // Render the 404 page using Pug
+  res.status(404).render("404", {
+    pageTitle: "Page Not Found",
+    path: "",
+    isAuthenticated: req.isLoggedIn,
+  }); // Render the 404 page using Pug
 };
