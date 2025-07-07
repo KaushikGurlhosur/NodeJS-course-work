0e0e0fd81ae3228ee6b2d1992089aaad01b04e2b
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-07 19:29:47 +0530

    Protecting routes - using middleware

--- Section 15 - Adding Authentication/Adding Authentication/routes/shop.js
+++ Section 15 - Adding Authentication/Adding Authentication/routes/shop.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const shopController = require("../controllers/shop");
 
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 router.get("/", shopController.getIndex);
@@ -10,15 +12,15 @@ router.get("/products", shopController.getProducts);
 
 router.get("/products/:productId", shopController.getProduct); // productId is a placeholder and can be used to get the product id from the URL
 
-router.get("/cart", shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
-router.post("/cart", shopController.postCart); // this is the route for the cart post request
+router.post("/cart", isAuth, shopController.postCart); // this is the route for the cart post request
 
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
-router.post("/create-order", shopController.postOrder);
+router.post("/create-order", isAuth, shopController.postOrder);
 
-router.get("/orders", shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
 // router.get("/checkout", shopController.getCheckout);
 
