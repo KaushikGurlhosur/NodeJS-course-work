d06b2450663ee17a5d1095b813dbb537f7fb5e64
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-09 19:54:03 +0530

    Understanding Validation

---
+++ Section 18 - Understanding Validation/Understanding Validation/controllers/auth.js
@@ -0,0 +1,268 @@
+const bcrypt = require("bcryptjs");
+
+const crypto = require("crypto");
+
+const dotenv = require("dotenv").config();
+
+const noddemailer = require("nodemailer");
+
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+const User = require("../models/user");
+
+const transporter = noddemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_API_KEY,
+    },
+  })
+);
+
+exports.getLogin = (req, res, next) => {
+  //   const isLoggedIn =
+  //     req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+
+  // console.log(req.session.isLoggedIn);
+
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: message,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: message,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.password;
+
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        req.flash(
+          "error",
+          "This emails already exists. Please use a different email address."
+        );
+        return res.redirect("/signup");
+      }
+
+      return bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          name: name,
+          email: email,
+          password: hashedPassword,
+          cart: { items: [] },
+        });
+        return user.save();
+      });
+    })
+    .then((result) => {
+      res.redirect("/login");
+      return transporter.sendMail({
+        to: email,
+        from: "kaushikgurlhosur@gmail.com",
+        subject: "Signup Succeeded!",
+        html: "<h1>You have successfully signed up!</h1>",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Invalid email or password");
+        return res.redirect("/login");
+      }
+
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.user = user; // Attach the user to the request object
+            req.session.isLoggedIn = true;
+            return req.session.save((err) => {
+              if (err) console.log(err);
+              transporter.sendMail({
+                to: email,
+                from: "kaushikgurlhosur@gmail.com",
+                subject: "Logged in Successfully!",
+                html: "<h1>You have successfully Logged In!</h1>",
+              });
+              res.redirect("/");
+            });
+          }
+          req.flash("error", "Invalid email or password");
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/login");
+    });
+
+  //   res.setHeader("Set-Cookie", "loggedIn=true");
+  //   res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10"); // Adding multiple
+  //   res.setHeader("Set-Cookie", "loggedIn=true; Secure"); // For https
+  //   res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly"); // for http
+  //   res.setHeader("Set-Cookie", "loggedIn=true; Domain=..."); // Where he cookie needs to be sent - for tracking purposes
+  //   res.redirect("/");
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+
+    const token = buffer.toString("hex"); // to convert Hexadecimal values to ASCII values
+
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000; // 1 hour
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        transporter.sendMail({
+          to: req.body.email,
+          from: "kaushikgurlhosur@gmail.com",
+          subject: "Password Reset",
+          html: ` <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a></p>
+          `,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let message = req.flash("error");
+
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "Setting New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+
+  const passwordToken = req.body.passwordToken;
+
+  let resetUser;
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+      transporter.sendMail({
+        to: resetUser.email,
+        from: "kaushikgurlhosur@gmail.com",
+        subject: "Changed your password",
+        html: ` <p>Your password has been reset. You can now log in with your new password..</p>
+          `,
+      });
+    })
+    .catch((err) => {
+      req.flash("error", "Something went wrong. Please try again.");
+      console.log(err);
+    });
+};
---
+++ Section 18 - Understanding Validation/Understanding Validation/controllers/shop.js
@@ -0,0 +1,158 @@
+const Order = require("../models/order");
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      console.log(products);
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+
+  // findById is from mongoose and we can pass a string and it will convert it to an ObjectId
+  Product.findById(prodId)
+    .then((product) => {
+      res.render("shop/product-detail", {
+        pageTitle: product.title,
+        product: product,
+        path: "/products",
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => console.log(err));
+
+  // Cart.getCart((cart) => {
+  //   Product.findAll().then((products) => {
+  //     const cartProducts = [];
+  //     for (let product of products) {
+  //       const cartProductData = cart.products.find(
+  //         (prod) => prod.id === product.id
+  //       );
+  //       if (cart.products.find((prod) => prod.id === product.id)) {
+  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
+  //       }
+  //     }
+  //     res.render("shop/cart", {
+  //       pageTitle: "Your Cart",
+  //       path: "/cart",
+  //       products: cartProducts,
+  //     });
+  //   });
+  // });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.findById(prodId).then((product) => {
+    return req.user.addToCart(product).then((result) => {
+      console.log(result);
+      res.redirect("/cart");
+    });
+  });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+
+  req.user
+    .removeFromCart(prodId)
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return {
+          quantity: i.quantity,
+          product: { ...i.productId._doc },
+        };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          email: req.user.email,
+          _id: req.user,
+        },
+        products: products,
+      });
+      order.save();
+    })
+    // .then(() => {
+    //   req.user.cart = { items: [] };
+    //   return req.user.save();
+    // })
+    .then(() => {
+      req.user.clearCart();
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getOrders = (req, res, next) => {
+  Order.find({ "user._id": req.session.user._id })
+    .then((orders) => {
+      // console.log(orders);
+      res.render("shop/orders", {
+        pageTitle: "Your Orders",
+        path: "/orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// exports.getCheckout = (req, res, next) => {
+//   res.render("shop/checkout", {
+//     pageTitle: "Checkout",
+//     path: "/checkout",
+//   });
+// };
