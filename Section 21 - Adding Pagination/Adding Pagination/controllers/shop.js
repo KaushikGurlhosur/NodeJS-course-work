63d9292d4bd7eeb3fcef973d545ac4e3f6ac7eb4
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-18 15:15:56 +0530

    Re-using the pagination in the products page

--- Section 21 - Adding Pagination/Adding Pagination/controllers/shop.js
+++ Section 21 - Adding Pagination/Adding Pagination/controllers/shop.js
@@ -9,14 +9,29 @@ const Product = require("../models/product");
 const ITEMS_PER_PAGE = 2; // Number of items per page for pagination.
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments() // Get the total number of products
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
-      console.log(products);
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
         isAuthenticated: req.session.isLoggedIn,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
