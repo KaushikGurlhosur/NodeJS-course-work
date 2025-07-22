d26325e7062e863ff875437d86098813df63602b
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-22 18:15:34 +0530

    Setting CORS - Cross Origin Resource Sharing

--- Section 24 - Understanding REST API - Basics/Understanding REST API - Basics/app.js
+++ Section 24 - Understanding REST API - Basics/Understanding REST API - Basics/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const feedRoutes = require("./routes/feed");
 
 const bodyParser = require("body-parser");
@@ -8,6 +10,20 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>Data</form>
 app.use(bodyParser.json()); // application/json
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
+// app.use(cors()); // Enable CORS for all routes
 
 // /feed/posts
 app.use("/feed", feedRoutes);
