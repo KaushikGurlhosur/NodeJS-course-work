476eb0cd87daefe53d0aaedc4dc67001ec29b968
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-07-25 14:25:53 +0530

    Fetching single post

--- Section 25 - Working with REST API's - The practical application/Understanding REST API - Basics/controllers/feed.js
+++ Section 25 - Working with REST API's - The practical application/Understanding REST API - Basics/controllers/feed.js
@@ -3,20 +3,18 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/kaushik.png",
-        creator: {
-          name: "Kaushik Gurlhosur",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: "Fetched posts successfully.", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500; // Internal Server Error
+      }
+      next(err); // Pass the error to the next middleware
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -59,5 +57,25 @@ exports.createPost = (req, res, next) => {
         err.statusCode = 500; // Internal Server Error
       }
       next(err); // Pass the error to the next middleware
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404; // Not Found
+        throw error;
+      }
+
+      res.status(200).json({ message: "Post fetched.", post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500; // Internal Server Error
+      }
+      next(err);
     });
 };
