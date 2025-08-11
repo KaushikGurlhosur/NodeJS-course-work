02f8535c08355e32698adacb1e921778fb1c957c
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-08-11 17:38:02 +0530

    Transforming Then Catch to Async await

--- Section 26 - Understanding Async Await in NodeJS/Understanding REST API - Basics/controllers/feed.js
+++ Section 26 - Understanding Async Await in NodeJS/Understanding REST API - Basics/controllers/feed.js
@@ -7,34 +7,30 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 const User = require("../models/user");
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
 
   let totalItems;
 
-  Post.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
+  try {
+    totalItems = await Post.find().countDocuments();
 
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((posts) => {
-      res.status(200).json({
-        message: "Fetched posts successfully.",
-        posts: posts,
-        totalItems: totalItems,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500; // Internal Server Error
-      }
-      next(err);
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+
+    res.status(200).json({
+      message: "Fetched posts successfully.",
+      posts: posts,
+      totalItems: totalItems,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500; // Internal Server Error
+    }
+    next(err);
+  }
 };
 
 exports.createPost = (req, res, next) => {
