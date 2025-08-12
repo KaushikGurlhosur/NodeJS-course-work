df593308e6e62508a01443ae1057edb5a8ae3b53
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-08-12 19:40:34 +0530

    Setting up Socket.io on server

--- Section 27 - Understanding WebSocket and Socket.io/Understanding WebSocket and Socket.io/app.js
+++ Section 27 - Understanding WebSocket and Socket.io/Understanding WebSocket and Socket.io/app.js
@@ -86,8 +86,13 @@ mongoose
   .connect(process.env.MONGODB_URL)
   .then((result) => {
     console.log("Connected to MongoDB");
-    app.listen(8080, () => {
+    const server = app.listen(8080, () => {
       console.log("Server is running on port 8080");
+    });
+    const io = require("socket.io")(server);
+
+    io.on("connection", (socket) => {
+      console.log("Client connected.");
     });
   })
   .catch((err) => {
